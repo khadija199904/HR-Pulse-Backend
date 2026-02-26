fff0d890d6f1a28b5fa99695361f006bbdcf4693
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-02-26 11:42:10 +0000

    HPP-12: Ajout les cruds

---
+++ src/api/crud/user_crud.py
@@ -0,0 +1,8 @@
+from src.core.security import password_hash
+from src.database.models.users import USER
+from src.api.schemas.user_schema import UserCreate
+
+def create_user (user : UserCreate):
+    hashed_password = password_hash(user.password)
+    new_user = USER(email= user.email,username=user.username,password_hash=hashed_password)
+    return new_user 
