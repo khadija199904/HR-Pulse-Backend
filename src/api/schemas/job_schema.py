4b28bdf5c4c3e157a46b5958008a2c481788a52d
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-02-27 09:16:19 +0000

    HPP-12: MOdifier le schema de jobskills

--- src/api/schemas/job_schema.py
+++ src/api/schemas/job_schema.py
@@ -1,10 +1,8 @@
 from pydantic import BaseModel
-from typing import List, Optional
 
 class JobSkillSchema(BaseModel):
-  
+    id: int
     job_title: str
-   
 
     class Config:
         from_attributes = True
