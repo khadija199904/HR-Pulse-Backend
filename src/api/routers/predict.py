ddb4f34a6727c4451d5195385c25ce27883598cf
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-03-02 09:41:37 +0000

    HPP-12: implement response validation for salary prediction

--- src/api/routers/predict.py
+++ src/api/routers/predict.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from src.api.schemas.predict_schema import JobProfile
+from src.api.schemas.predict_schema import JobProfile , PredictionResponse
 from src.api.services.ml_service import get_prediction
 
 router = APIRouter(prefix="/predict", tags=["Prédiction"])
@@ -7,11 +7,22 @@ router = APIRouter(prefix="/predict", tags=["Prédiction"])
 
 
 
-@router.post("/predict")
+@router.post("/predict",response_model=PredictionResponse)
 async def predict_salary(profile: JobProfile):
-   
-    prediction = get_prediction(profile)
-    return {
-        "salary_estimate": round(prediction, 2),
+    
+    profile = JobProfile(**profile)
+    try:
+        predicted_salary = get_prediction(profile)
+        rounded_salary = round(predicted_salary, 2)
         
-    }
+        return {
+            "salary_estimate": rounded_salary,
+            
+        }
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, 
+            detail=f"Erreur lors de la prédiction : {str(e)}"
+        )
+       
